2076c5415bf5852a920ad5b3963b567094a0f63e
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-11-13 08:06:54 -0400

    feat: adiciona model com a orientação a objetos do projeto

--- Models/TodolistModel.cs
+++ Models/TodolistModel.cs
@@ -1,18 +1,35 @@
 
+using System.Runtime.InteropServices;
+
 namespace Todolist.Models;
 
 public class TodolistModel{
-    public TodolistModel(string title, string text, string status){
+    public TodolistModel(string title, string text){
         Id = Guid.NewGuid();
         Title = title;
         Text = text;
-        Status = status;
     }
 
     public Guid Id {get; set;}
     public String Title{get; private set;} = String.Empty;
 
     public String Text{get; private set;} = String.Empty;
-    public String Status{get; private set;} = String.Empty;
+    public String Status{get; private set;} = "ativo";
+
+    public void ChangeTitle(string title)
+    {
+        Title = title;
+    }
+    
+    public void ChangeText(string text)
+    {
+        Text = text;
+    }
+    
+    public void ChangeStatus()
+    {
+        Status = "feito";
+    }
+
     
 }
